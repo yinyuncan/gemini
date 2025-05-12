992302e15371f9594578ac4006a5bb8bd89455b4
Mr Can <yinyuncan@gmail.com>
2025-05-13 03:23:13 +0800

    Update worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -227,9 +227,9 @@ async function handleGenerateImage (req, apiKey) {
       }
     };
   
-    const response = await fetch(`${BASE_URL}/${API_VERSION}/models/${MODEL}:generateContent?key=${apiKey}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation:generateContent?key=${apiKey}`, {
       method: "POST",
-      headers: makeHeaders(apiKey, { "Content-Type": "application/json" }),
+      headers: {"Content-Type": "application/json" },
       body: JSON.stringify(body)
     });
   
